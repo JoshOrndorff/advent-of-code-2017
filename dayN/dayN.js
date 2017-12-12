0b73cec9b41560d6243550dcfe6db54ad1dea75f
Josh Orndorff <admin@joshorndorff.com>
2017-12-12 01:06:57 -0500

    Update javascript event handling and namespacing.

--- dayN/dayN.js
+++ dayN/dayN.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var part1 = {
-  // Globals here
+document.addEventListener("DOMContentLoaded", function(event){
 
-  solve: function(){
+  // Attach event listeners for each button.
+  document.getElementById("button1").addEventListener("click", solvePart1)
+  document.getElementById("button2").addEventListener("click", solvePart2)
+
+  function solvePart1(){
     // Parse the input
     var inp = parseMultilineInput()
     var inp = parseOnelineInput()
@@ -19,43 +22,28 @@ var part1 = {
     // Display the answer
     document.getElementById("answer1").innerHTML = "fill me in"
   }
-}
 
-var part2 = {
-  // Globals here
-
-  solve: function(){
-    // Parse the input
-    var inp = parseMultilineInput()
-    var inp = parseOnelineInput()
-
-    // Cast to numbers
-    inp = inp.map(Number)
-
-    //
-
-
-
+  function solvePart2(){
 
     // Display the answer
     document.getElementById("answer2").innerHTML = "fill me in"
   }
-}
 
+  function parseMultilineInput(){
+    var lines = document.getElementById("inp").value.split("\n")
 
-function parseMultilineInput(){
-  var lines = document.getElementById("inp").value.split("\n")
+    // Cut off empty string caused by eg trailing newline
+    if (lines[lines.length - 1] == ""){
+      lines = lines.slice(0, -1) // chop off empty one at end
+    }
 
-  // Cut off empty string caused by eg trailing newline
-  if (lines[lines.length - 1] == ""){
-    lines = lines.slice(0, -1) // chop off empty one at end
+    return lines
   }
 
-  return lines
-}
+  function parseOnelineInput(){
+    var inp = document.getElementById("inp").value.split("\t")
 
-function parseOnelineInput(){
-  var inp = document.getElementById("inp").value.split("\t")
+    return inp
+  }
 
-  return inp
-}
+})
