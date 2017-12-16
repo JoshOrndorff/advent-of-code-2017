f678fc37681c1992bf99983b5d18b2603a578ae2
Josh Orndorff <admin@joshorndorff.com>
2017-12-15 23:59:20 -0500

    Updated JS template

--- dayN/dayN.js
+++ dayN/dayN.js
@@ -6,13 +6,20 @@ document.addEventListener("DOMContentLoaded", function(event){
   document.getElementById("button1").addEventListener("click", solvePart1)
   document.getElementById("button2").addEventListener("click", solvePart2)
 
+  // Global common problem parameters
+
+
+  // Global my problem parameters
+  var inp
+
+
+  /**
+   * Solves the first part of the problem and displays answer in the DOM
+   */
   function solvePart1(){
     // Parse the input
-    var inp = parseMultilineInput()
-    var inp = parseOnelineInput()
-
-    // Cast to numbers
-    inp = inp.map(Number)
+    inp = parseMultilineInput()
+    inp = document.getElementById("inp").value.split("\t")
 
     //
 
@@ -23,12 +30,22 @@ document.addEventListener("DOMContentLoaded", function(event){
     document.getElementById("answer1").innerHTML = "fill me in"
   }
 
+
+  /**
+   * Solves the second part of the problem and displays answer in the DOM
+   */
   function solvePart2(){
 
     // Display the answer
     document.getElementById("answer2").innerHTML = "fill me in"
   }
 
+
+  /**
+   * Reads the input from the textarea in the DOM. Cuts of trailing blank line
+   * when it is present
+   * @return List of lines from the textarea
+   */
   function parseMultilineInput(){
     var lines = document.getElementById("inp").value.split("\n")
 
@@ -40,10 +57,5 @@ document.addEventListener("DOMContentLoaded", function(event){
     return lines
   }
 
-  function parseOnelineInput(){
-    var inp = document.getElementById("inp").value.split("\t")
-
-    return inp
-  }
 
 })
